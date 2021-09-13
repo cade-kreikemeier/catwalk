14274285788f5638a690284470f57a09aa394156
Trevor Settles <roboteng@gmail.com>
2021-09-13 16:19:59 -0600

    improved image sizing

--- client/src/related/carouselCard.tsx
+++ client/src/related/carouselCard.tsx
@@ -14,15 +14,14 @@ const cardStyle = {
 
 const imageStyle: Record<string, unknown> = {
   height: '150px',
+  width: 'auto',
   objectFit: 'contain',
   overflow: 'hidden'
 };
 
 const CarouselCard: React.FC<CarouselCardProps> = (props) => {
   return <div style={cardStyle}>
-    <div style={imageStyle} >
-      <img src="https://i.kym-cdn.com/photos/images/newsfeed/000/920/899/715.jpg" />
-    </div>
+    <img style={imageStyle} src="https://i.kym-cdn.com/photos/images/newsfeed/000/920/899/715.jpg" />
     <p>{props.category}</p>
     <p>{props.title}</p>
   </div>;
