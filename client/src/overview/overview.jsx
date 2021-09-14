f419eab0bb273efcfb87ef314cd31b8292ac6e87
louis09392003 <louis09392003@gmail.com>
2021-09-14 09:36:21 -0600

    clear up comment

--- client/src/overview/overview.jsx
+++ client/src/overview/overview.jsx
@@ -4,14 +4,6 @@ import ImageGallery from './subComponents/ImageGallery.jsx';
 import ProductInfo from './subComponents/ProductInfo.jsx';
 import StyleSelector from './subComponents/StyleSelector.jsx';
 
-
-// const divStyle = {
-//   display: 'grid',
-//   // grid-gap: '5%',
-//   grid-template-columns: "4fr 40% 1fr 30% 4fr",
-//   grid-template-rows: "4fr 20% 1fr 10% 1fr 10% 4fr"
-// };
-
 export default function OverView(props) {
   return (
     <div className="overViewContainer">
--- client/src/overview/subComponents/AddToCart.jsx
+++ client/src/overview/subComponents/AddToCart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function AddToCart() {
   return (
     <div className='addToCart'>
-      <div className='Add2Bag'>ADD TO BAG</div>
+      <div className='AddToBag'>ADD TO BAG</div>
       <div className='favBtn'>⭐️</div>
     </div>
   )
