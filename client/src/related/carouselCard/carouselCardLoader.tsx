87664946908774ce4e8dd2c66c82b5211ee0f3e9
Trevor Settles <roboteng@gmail.com>
2021-09-18 16:32:15 -0600

    corrected styles to not be a list, now thumbnails display

--- client/src/related/carouselCard/carouselCardLoader.tsx
+++ client/src/related/carouselCard/carouselCardLoader.tsx
@@ -2,22 +2,17 @@ import React from 'react';
 import { apiRequest } from '../../utils/apiRequests';
 import loadState from '../../utils/loadState';
 import CarouselCard from './carouselCard';
+import findImageUrl from './findImageUrl';
 
 interface CarouselCardLoaderProps {
   id: number
 }
 
 const CarouselCardLoader: React.FC<CarouselCardLoaderProps> = ({ id }) => {
-  const styles = loadState(apiRequest.getProductStyles(id), []);
-  let imageUrl: string;
-  if (styles.length === 0) {
-    imageUrl = '';
-  } else {
-    imageUrl = styles[0].results[0].photos[0].thumbnail_url;
-  }
+  const style = loadState(apiRequest.getProductStyles(id), null);
   return <React.Fragment>
     <CarouselCard
-      imageUrl={imageUrl}
+      imageUrl={findImageUrl(style)}
       metaData={{ category: 'Category', name: 'Name', price: (id + '.00') }}
       rating={4}
       actionCallback={console.log}
--- client/src/related/carouselCard/findImageUrl.tsx
+++ client/src/related/carouselCard/findImageUrl.tsx
@@ -1,7 +1,11 @@
-import { style } from "../../models/style.interface";
+import { style } from '../../models/style.interface';
 
-function findImageUrl(styles: style[]): string {
-  return '';
+function findImageUrl(style: style): string {
+  if (style === null) {
+    return '';
+  } else {
+    return style.results[0].photos[0].thumbnail_url;
+  }
 }
 
 export default findImageUrl;
