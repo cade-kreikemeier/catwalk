2945a843ef2fa18154117a6b6fba66b494e09f6a
Trevor Settles <roboteng@gmail.com>
2021-09-18 14:42:31 -0600

    added placeholder for cors

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const ExpressCache = require('express-cache-middleware');
 const cacheManager = require('cache-manager');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { API_TOKEN, API_URL } = require('../apiConfig/config.ts');
+// const cors = require('cors');
 
 const port = 9001;
 const app = express();
@@ -15,6 +16,14 @@ const cacheMiddleware = new ExpressCache(
 );
 
 cacheMiddleware.attach(app);
+
+// app.use(cors());
+
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
