9bf15066e0c769807aca252cbe232deadfb1cdde
louis09392003 <louis09392003@gmail.com>
2021-09-20 11:56:03 -0600

    basic thumbnail click not working

--- client/src/overview/overview.jsx
+++ client/src/overview/overview.jsx
@@ -3,18 +3,14 @@ import AddToCart from './subComponents/AddToCart.jsx';
 import ImageGallery from './subComponents/ImageGallery.jsx';
 import ProductInfo from './subComponents/ProductInfo/ProductInfo.jsx';
 import StyleSelector from './subComponents/StyleSelector.jsx';
-// import Contexts from '../contexts/Contexts.tsx';
 
-let test = 'str';
 export default function OverView(props) {
   return (
     <div className="overViewContainer">
       <div className="overView">
         <ImageGallery />
         <ProductInfo />
-        <StyleSelector
-          testStr={test}
-        />
+        <StyleSelector />
         <AddToCart />
       </div>
     </div>
