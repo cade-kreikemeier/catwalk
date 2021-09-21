8679cb1e6a117353c8a99c4b0b7bba38bc5fdbaa
Trevor Settles <roboteng@gmail.com>
2021-09-21 12:08:41 -0600

    removed console.log

--- client/src/related/carouselCard/carouselCardLoader.tsx
+++ client/src/related/carouselCard/carouselCardLoader.tsx
@@ -11,7 +11,6 @@ interface CarouselCardLoaderProps {
 const CarouselCardLoader: React.FC<CarouselCardLoaderProps> = ({ id }) => {
   const style = loadState(apiRequest.getProductStyles(id), null);
   const product = loadState(apiRequest.getProductById(id), null);
-  console.log(style);
   return <React.Fragment>
     <CarouselCard
       imageUrl={findImageUrl(style)}
