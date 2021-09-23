c8ce0a60bf141508225f3d2abfa0b3bc8cd12be6
Trevor Settles <roboteng@gmail.com>
2021-09-23 17:34:25 -0600

    started comparison creation

--- client/src/related/comparisonsModal/Context.tsx
+++ client/src/related/comparisonsModal/Context.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 
-interface comparison {
-  [characterist: string] : {
+export interface comparison {
+  [characteristic: string] : {
     mainValue: number | null,
     otherValue: number | null,
   }
