0c2b3c23ece8e1f9e11c07e3e39bddddd5429927
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-09-22 20:45:49 -0600

    Added checkmark icon to recommended span

--- client/src/reviews/ReviewTile.tsx
+++ client/src/reviews/ReviewTile.tsx
@@ -20,7 +20,10 @@ const ReviewTile: FC<{ review: review }> = ({ review }) => {
       <div className="reviewBody">
         {review.body}
       </div>
-      <div className="reviewRecommend">{review.recommend ? 'Recommened' : null}</div>
+      <div className="reviewRecommend">{review.recommend
+        ? <span className={'far fa-check-circle checkIcon'}> I recommend this product</span>
+        : null}
+      </div>
       {review.response ? <div className="reviewResponse"><strong>{review.response}</strong></div> : null}
       <div className="reviewHelpfulness">{review.helpfulness}</div>
     </div>
