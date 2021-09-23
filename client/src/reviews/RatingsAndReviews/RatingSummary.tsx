ea03324718bf9dab45b2acf9048a263b4341f02c
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-09-23 17:13:50 -0600

    Added total review count to RatingSummary.tsx

--- client/src/reviews/RatingsAndReviews/RatingSummary.tsx
+++ client/src/reviews/RatingsAndReviews/RatingSummary.tsx
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react';
 import { ReviewsMetadataContext } from '../../contexts/Contexts';
 import calAvgRating from '../../utils/calAvgRating';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import ReviewStar from '../../utils/ReviewStar.jsx';
+
 
 const RatingSummary: React.FC = () => {
-  const { reviewsMetadata } = useContext(ReviewsMetadataContext) || {};
+  const { reviewsMetadata, reviewCount } = useContext(ReviewsMetadataContext) || {};
 
   if (reviewsMetadata?.ratings && Object.keys(reviewsMetadata.ratings).length) {
     return (
       <div className="ratingSummary">
         <h2>{calAvgRating(reviewsMetadata.ratings, 0.1).toString().slice(0, 3)}</h2>
+        <ReviewStar rating={calAvgRating(reviewsMetadata.ratings)} />
+        <span>{reviewCount} Ratings</span>
       </div>
     );
   } else {
