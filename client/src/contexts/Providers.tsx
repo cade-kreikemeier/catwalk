e13704668396360473965aee99659405f66225dd
louis09392003 <louis09392003@gmail.com>
2021-09-22 21:55:24 -0600

    basic preview finished

--- client/src/contexts/Providers.tsx
+++ client/src/contexts/Providers.tsx
@@ -10,7 +10,7 @@ type Props = {
   children: ReactNode;
 };
 
-const productId = 44388;
+const productId = 44392;
 
 export function ProductsProvider({ children }: Props): ReactElement {
   const [products, setProducts] = useState<product[]>([]);
--- client/src/overview/subComponents/ImageGallery.jsx
+++ client/src/overview/subComponents/ImageGallery.jsx
@@ -5,14 +5,19 @@ import { StyleIdxContext } from '../overview.jsx';
 export default function ImageGallery({ currentProductStyles }) {
   const StyleIdxContextData = useContext(StyleIdxContext);
   const stylePics = [];
+  const previewPics = [];
   const [currentSlide, setCurrentSlide] = useState(0);
-
+  const [currentPreviewIdx, setCurrentPreviewIdx] = useState(0);
   let slideLength = 0;
   let currentStyleIdx;
+  // let setCurrentStyleIdx;
   if (StyleIdxContextData && currentProductStyles) {
     ({ currentStyleIdx } = StyleIdxContextData);
     currentProductStyles.results[currentStyleIdx].photos.forEach((photo) => {
       stylePics.push(photo.url);
+    });
+    currentProductStyles.results[currentStyleIdx].photos.forEach((photo) => {
+      previewPics.push(photo.thumbnail_url);
     });
     slideLength = stylePics.length;
   }
@@ -20,17 +25,38 @@ export default function ImageGallery({ currentProductStyles }) {
   const changeSlide = (event, handle) => {
     if (handle === 'next') {
       setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+      setCurrentPreviewIdx(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
     } else if (handle === 'prev') {
       setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+      setCurrentPreviewIdx(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
     }
+  };
+
+  const previewClicked = (event, index) => {
+    setCurrentPreviewIdx(index);
+    setCurrentSlide(index);
   };
 
   useEffect(() => {
     setCurrentSlide(0);
+    setCurrentPreviewIdx(0);
   }, [currentStyleIdx]);
 
   return (
     <div className='imageGallery'>
+      <div className='previewContainer'>
+        {previewPics.map((previewPic, index) => {
+          return (
+            <span key={'previewPic' + index} className='previewBox' onClick={() => previewClicked(event, index)}
+            style={{
+              border: index === currentPreviewIdx ? '2px solid white ' : '2px dashed black '
+            }}>
+              <img src={previewPic} className='preview'></img>
+            </span>
+          );
+        })}
+      </div>
+
       { currentSlide === 0
         ? null
         : <button className={ 'fas fa-chevron-left left-arrow arrow'} onClick={ () => changeSlide(event, 'prev') } />}
