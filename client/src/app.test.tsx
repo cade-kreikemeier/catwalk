168bddc350fb3aa2009327a9f20ee747a519a562
Trevor Settles <roboteng@gmail.com>
2021-09-24 15:01:27 -0600

    added sample render of App, for test coverage

---
+++ client/src/app.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { act, render } from "@testing-library/react";
+import App from './App';
+import React from 'react';
+
+test('testing the app', () => {
+  act(() => {
+    render(<App></App>);
+  });
+});
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Route path="/products/:product_id" component={ProductPage} />
-      <Route exact path="/" component={Home} />
-    </Router>
-  );
-};
+import App from './app';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+export default App;
