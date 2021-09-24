282ab683e1a2ca9ef565f6fc887d03eaf0f24928
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-09-23 19:27:10 -0600

    Fixed arithmetic in ratingMeters

--- client/src/reviews/RatingsAndReviews/RatingBreakdown.tsx
+++ client/src/reviews/RatingsAndReviews/RatingBreakdown.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ReviewsMetadataContext } from '../../contexts/Contexts';
 
-const RatingBreakdown: React.FC = () => (
-  <div className="ratingBreakdown">
-    <h4>Rating Breakdown</h4>
-  </div>
-);
+const RatingBreakdown: React.FC = () => {
+  const { reviewsMetadata, reviewCount } = useContext(ReviewsMetadataContext) || {};
+
+  const ratingMeters = [];
+
+
+  if (reviewCount && reviewsMetadata?.ratings) {
+    for (let i = 5; i > 0; i--) {
+      ratingMeters.push(<div>
+        <label htmlFor={`${i}star`}>{i} Stars </label>
+        <meter id={`${i}star`}
+          value={parseInt(reviewsMetadata?.ratings[i]) / reviewCount || 0}
+        >{i} Star Ratings</meter>
+      </div>);
+    }
+  }
+
+  return (
+    <div className="ratingBreakdown">
+      <h4>Rating Breakdown</h4>
+      {ratingMeters}
+    </div>
+  );
+};
 
 export default RatingBreakdown;
