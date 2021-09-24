ad1310d8decd34ee82b3368539c4b0657e1f7282
Trevor Settles <roboteng@gmail.com>
2021-09-23 20:57:43 -0600

    took out useless if statement

--- client/src/related/comparisonsModal/buildComparisons.ts
+++ client/src/related/comparisonsModal/buildComparisons.ts
@@ -3,22 +3,20 @@ import { comparison } from './Context';
 
 export default function buildComparisons(productFrom: characteristics | null, productTo: characteristics | null): comparison {
   const comparison: comparison = {};
-  if (productFrom && productTo) {
-    for (const characteristic in productFrom) {
+  for (const characteristic in productFrom) {
+    comparison[characteristic] = {
+      mainValue: productFrom[characteristic].value,
+      otherValue: null
+    };
+  }
+  for (const characteristic in productTo) {
+    if (comparison[characteristic]) {
+      comparison[characteristic].otherValue = productTo[characteristic].value;
+    } else {
       comparison[characteristic] = {
-        mainValue: productFrom[characteristic].value,
-        otherValue: null
+        mainValue: null,
+        otherValue: productTo[characteristic].value
       };
-    }
-    for (const characteristic in productTo) {
-      if (comparison[characteristic]) {
-        comparison[characteristic].otherValue = productTo[characteristic].value;
-      } else {
-        comparison[characteristic] = {
-          mainValue: null,
-          otherValue: productTo[characteristic].value
-        };
-      }
     }
   }
   return comparison;
