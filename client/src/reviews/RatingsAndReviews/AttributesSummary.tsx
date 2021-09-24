954c392fa951ca40c546f66d519fbdf875deb5c6
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-09-23 22:18:56 -0600

    Added meter inputs to RatingBreakdown.tsx

--- client/src/reviews/RatingsAndReviews/AttributesSummary.tsx
+++ client/src/reviews/RatingsAndReviews/AttributesSummary.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ReviewsMetadataContext } from '../../contexts/Contexts';
 
-const AttributesSummary: React.FC = () => (
-  <div className="attributesSummary">
-    <h4>Attributes Summary</h4>
-  </div>
-);
+const AttributesSummary: React.FC = () => {
+  const { reviewsMetadata } = useContext(ReviewsMetadataContext) || {};
+
+  if (reviewsMetadata) {
+    const characteristics = [];
+    for (const char in reviewsMetadata.characteristics) {
+      characteristics.push(<div key={char}>
+        <label htmlFor={char}>{char}</label>
+        <input readOnly
+          type="range"
+          min="0"
+          max="100"
+          step="1"
+          disabled={true}
+          value={parseInt(reviewsMetadata.characteristics[char].value) * 20}>
+        </input>
+      </div>);
+    };
+
+    return (
+      <div className="attributesSummary">
+        <h4>Attributes Summary</h4>
+        {characteristics}
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
 export default AttributesSummary;
--- client/src/reviews/RatingsAndReviews/RatingBreakdown.tsx
+++ client/src/reviews/RatingsAndReviews/RatingBreakdown.tsx
@@ -4,26 +4,31 @@ import { ReviewsMetadataContext } from '../../contexts/Contexts';
 const RatingBreakdown: React.FC = () => {
   const { reviewsMetadata, reviewCount } = useContext(ReviewsMetadataContext) || {};
 
-  const ratingMeters = [];
+  if (reviewsMetadata && reviewCount) {
+    const recommended = parseInt(reviewsMetadata.recommended.true) || 0;
+    const notRecommended = parseInt(reviewsMetadata.recommended.false) || 0;
+    const recommendedPercentage = Math.floor((recommended / (recommended + notRecommended)) * 100);
 
+    const ratingMeters = [];
 
-  if (reviewCount && reviewsMetadata?.ratings) {
     for (let i = 5; i > 0; i--) {
-      ratingMeters.push(<div>
+      ratingMeters.push(<div key={i}>
         <label htmlFor={`${i}star`}>{i} Stars </label>
         <meter id={`${i}star`}
           value={parseInt(reviewsMetadata?.ratings[i]) / reviewCount || 0}
         >{i} Star Ratings</meter>
       </div>);
     }
-  }
 
-  return (
-    <div className="ratingBreakdown">
-      <h4>Rating Breakdown</h4>
-      {ratingMeters}
-    </div>
-  );
+    return (
+      <div className="ratingBreakdown">
+        {ratingMeters}
+        <span>{recommendedPercentage}% of reviewers recommend this product</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default RatingBreakdown;
