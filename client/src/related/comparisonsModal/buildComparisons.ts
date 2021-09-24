fbcf801cfabc9540b00b5c9adcc6405cd90896e1
Trevor Settles <roboteng@gmail.com>
2021-09-24 16:44:30 -0600

    handled case for first object having a null value

--- client/src/related/comparisonsModal/buildComparisons.ts
+++ client/src/related/comparisonsModal/buildComparisons.ts
@@ -4,10 +4,12 @@ import { comparison } from './Context';
 export default function buildComparisons(productFrom: characteristics | null, productTo: characteristics | null): comparison {
   const comparison: comparison = {};
   for (const characteristic in productFrom) {
-    comparison[characteristic] = {
-      mainValue: productFrom[characteristic].value,
-      otherValue: null
-    };
+    if (productFrom[characteristic].value !== null) {
+      comparison[characteristic] = {
+        mainValue: productFrom[characteristic].value,
+        otherValue: null
+      };
+    }
   }
   for (const characteristic in productTo) {
     comparison[characteristic] = {
