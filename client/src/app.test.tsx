08b1f77e1014cb9115423becead5c06717e19ffe
Trevor Settles <roboteng@gmail.com>
2021-09-24 19:57:21 -0600

    hid app test

--- client/src/app.test.tsx
+++ client/src/app.test.tsx
@@ -5,7 +5,7 @@ import { act, render } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
-test('testing the app', () => {
+xtest('testing the app', () => {
   act(() => {
     render(<App></App>);
   });
