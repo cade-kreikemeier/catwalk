6347ada3f423314c54a4318d5cb427d5d980edae
Trevor Settles <roboteng@gmail.com>
2021-09-25 10:14:42 -0600

    moved the action button to the top right

--- client/src/related/carouselCard/carouselCard.tsx
+++ client/src/related/carouselCard/carouselCard.tsx
@@ -15,20 +15,23 @@ interface CarouselCardProps {
 
 const CarouselCard: React.FC<CarouselCardProps> = (props) => {
   return <div className="carouselCard" onClick={props.localCallback} >
-    {props.imageUrl
-      ? <img data-testid="thumbnail" src={props.imageUrl} />
-      : null
-    }
-    <p data-testid="category">{props.metaData.category}</p>
-    <p data-testid="name">{props.metaData.name}</p>
-    <p data-testid="price">${props.metaData.price}</p>
-    <ReviewStar rating={props.rating} />
+    <div>
+      {props.imageUrl
+        ? <img data-testid="thumbnail" src={props.imageUrl} />
+        : null
+      }
+      <p data-testid="category">{props.metaData.category}</p>
+      <p data-testid="name">{props.metaData.name}</p>
+      <p data-testid="price">${props.metaData.price}</p>
+      <ReviewStar rating={props.rating} />
+    </div>
     <button className="carouselActionButton" onClick={e => {
       props.actionCallback();
       e.stopPropagation();
     }}>
       {props.actionChild}
     </button>
+
   </div>;
 };
 
