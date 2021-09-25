4682b6f800f027e0d54a34348a5d38afec377b2b
Trevor Settles <roboteng@gmail.com>
2021-09-25 14:47:15 -0600

    shortened number to 1 decimal place

--- client/src/related/comparisonsModal/buildComparisons.ts
+++ client/src/related/comparisonsModal/buildComparisons.ts
@@ -5,17 +5,29 @@ export default function buildComparisons(productFrom: characteristics | null, pr
   const comparison: comparison = {};
   for (const characteristic in productFrom) {
     if (productFrom[characteristic].value !== null) {
+      let val: string;
+      if (!Number.isNaN(Number(productFrom[characteristic].value))) {
+        val = Number(productFrom[characteristic].value).toFixed(1);
+      } else {
+        val = productFrom[characteristic].value || '';
+      }
       comparison[characteristic] = {
-        mainValue: productFrom[characteristic].value,
+        mainValue: val,
         otherValue: null
       };
     }
   }
   for (const characteristic in productTo) {
     if (productTo[characteristic].value !== null) {
+      let val: string;
+      if (!Number.isNaN(Number(productTo[characteristic].value))) {
+        val = Number(productTo[characteristic].value).toFixed(1);
+      } else {
+        val = productTo[characteristic].value || '';
+      }
       comparison[characteristic] = {
         mainValue: comparison[characteristic]?.mainValue || null,
-        otherValue: productTo[characteristic].value
+        otherValue: val
       };
     }
   }
